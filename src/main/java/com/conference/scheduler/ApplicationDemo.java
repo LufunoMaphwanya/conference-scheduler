e5134f91c5bc79d20046e7fb5ceb9ee5855ac530
lufuno maphwanya <lcmaphwanya@gmail.com>
2017-11-17 13:00:09 +0200

    Hello conference commit.

---
+++ src/main/java/com/conference/scheduler/ApplicationDemo.java
@@ -0,0 +1,14 @@
+ package com.conference.scheduler;
+
+ import java.text.ParseException;
+
+ public class ApplicationDemo {
+
+
+     public static void main(String[] args) throws ParseException {
+            System.out.println("Hello conference project");
+
+
+     }
+ }
+
