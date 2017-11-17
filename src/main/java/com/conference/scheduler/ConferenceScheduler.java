56e582ce4fb5bb6b168a7dbfd0042ad905756f89
lufuno maphwanya <lcmaphwanya@gmail.com>
2017-11-17 13:22:42 +0200

    adds conference scheduler.

---
+++ src/main/java/com/conference/scheduler/ConferenceScheduler.java
@@ -0,0 +1,63 @@
+package com.conference.scheduler;
+
+import java.util.ArrayList;
+import java.util.Stack;
+import static com.conference.scheduler.SESSION_TYPE.AFTERNOON;
+import static com.conference.scheduler.SESSION_TYPE.MORNING;
+
+public class ConferenceScheduler {
+    public Conference schedule(ArrayList<Talk> talks) {
+
+        Conference.ConferenceBuilder conferenceBuilder = new Conference.ConferenceBuilder()
+                .title("Conference ##");
+
+        Stack<Talk> talkStack = sortAndConvertToStack(talks);
+        List<Track> tracks = new ArrayList<>();
+        Track.TrackBuilder trackBuilder = new Track.TrackBuilder();
+        Session morningSession = new Session(MORNING);
+        Session afternoonSession = new Session(AFTERNOON);
+        int talkIterator = 1;
+
+        while (!talkStack.empty()) {
+            Talk currentTalk = talkStack.pop();
+
+            if ((morningSession.fitsIntoSession(currentTalk) || afternoonSession.fitsIntoSession(currentTalk))) {
+                if (morningSession.fitsIntoSession(currentTalk)) {
+                    morningSession.insertTalk(currentTalk);
+                } else if (afternoonSession.fitsIntoSession(currentTalk)) {
+                    afternoonSession.insertTalk(currentTalk);
+                }
+            } else {
+                trackBuilder.morningSession(morningSession);
+                trackBuilder.afternoonSession(afternoonSession);
+                trackBuilder.title(String.format("Track %d", talkIterator));
+                tracks.add(trackBuilder.build());
+
+                morningSession = new Session(MORNING);
+                afternoonSession = new Session(AFTERNOON);
+                trackBuilder = new Track.TrackBuilder();
+                talkIterator++;
+            }
+
+        }
+        conferenceBuilder.tracks(tracks);
+        return conferenceBuilder.build();
+    }
+
+    private Stack<Talk> sortAndConvertToStack(ArrayList<Talk> talks) {
+        talks.sort(
+                (t1, t2) -> {
+                    if (t1.getDuration() == t2.getDuration()) {
+                        return 0;
+                    } else {
+                        return (t1.getDuration() < t2.getDuration()) ? -1 : 1;
+                    }
+                }
+        );
+
+        Stack<Talk> stack = new Stack();
+        talks.forEach(stack::push);
+        return stack;
+    }
+
+}
